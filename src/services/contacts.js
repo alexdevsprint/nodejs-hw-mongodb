f913c45fa10968ca25ed3e702755df9ff96a986e
alexdevsprint <alexdevsprint@gmail.com>
2025-07-23 14:08:34 +0300

    commit

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,7 +2,10 @@ import { ContactsCollection } from '../db/models/contacts.js';
 
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async ({page, perPage}) => {
+import { SORT_ORDER } from '../constants/index.js';
+
+export const getAllContacts = async ({page = 1, perPage = 10, sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',} ) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
   const contactsQuery = ContactsCollection.find();
@@ -10,7 +13,7 @@ export const getAllContacts = async ({page, perPage}) => {
     .merge(contactsQuery)
     .countDocuments();
 
-   const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+   const contacts = await contactsQuery.skip(skip).limit(limit).sort({ [sortBy]: sortOrder }).exec();
 
    const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
